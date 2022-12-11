28828266e3eaa271b115dcccf6eb21948fbc874c
FabijanC <fabijan.corak@gmail.com>
2022-12-11 12:32:57 +0100

    Reorganize input data; Introduce testing

--- year2022/scripts/create_day.sh
+++ year2022/scripts/create_day.sh
@@ -7,5 +7,5 @@ DAY="$1"
 mkdir -p "src/bin/day$DAY-"{part1,part2}
 code -r "src/bin/day$DAY-"{part1,part2}"/main.rs"
 
-mkdir "input/day$DAY"
-code -r "input/day$DAY/"{sample.txt,input.txt}
+mkdir "data/day$DAY"
+code -r "data/day$DAY/"{sample.txt,sample-output-part1.txt,sample-output-part2.txt,input.txt}
