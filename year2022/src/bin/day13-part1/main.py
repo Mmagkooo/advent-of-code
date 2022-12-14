9358bdeaf29e96ad6828a255bc369afe3a607cec
FabijanC <fabijan.corak@gmail.com>
2022-12-15 00:24:15 +0100

    Improve python solutions for day13

--- year2022/src/bin/day13-part1/main.py
+++ year2022/src/bin/day13-part1/main.py
@@ -7,30 +7,19 @@ def compare(left: List, right: List):
         left_el_is_int = isinstance(left_el, int)
         right_el_is_int = isinstance(right_el, int)
         if left_el_is_int and right_el_is_int:
-            if left_el < right_el:
-                return True
-            if left_el > right_el:
-                return False
-        elif not left_el_is_int and not right_el_is_int:
-            list_comparison = compare(left_el, right_el)
-            if list_comparison is not None:
-                return list_comparison
+            if left_el != right_el:
+                return left_el - right_el
         else:
-            list_comparison = (
-                compare([left_el], right_el)
-                if left_el_is_int
-                else compare(left_el, [right_el])
-            )
-            if list_comparison is not None:
-                return list_comparison
-    
-    if len(left) < len(right):
-        return True
-    elif len(left) > len(right):
-        return False
+            if left_el_is_int:
+                left_el = [left_el]
+            if right_el_is_int:
+                right_el = [right_el]
 
-    # lists are equal
-    return None
+            list_comparison = compare(left_el, right_el)
+            if list_comparison:
+                return list_comparison
+
+    return len(left) - len(right)
 
 
 def main():
@@ -40,12 +29,14 @@ def main():
     for line_i in range(0, len(lines), 3):
         left = eval(lines[line_i])
         right = eval(lines[line_i + 1])
-        
-        if compare(left, right) == True:
+
+        if compare(left, right) < 0:
             i = line_i // 3 + 1
+            print("Correct index:", i)
             sol += i
 
     print(sol)
+
 
 if __name__ == "__main__":
     main()
--- year2022/src/bin/day13-part2/main.py
+++ year2022/src/bin/day13-part2/main.py
@@ -8,30 +8,20 @@ def compare(left: List, right: List):
         left_el_is_int = isinstance(left_el, int)
         right_el_is_int = isinstance(right_el, int)
         if left_el_is_int and right_el_is_int:
-            if left_el < right_el:
-                return -1
-            if left_el > right_el:
-                return 1
-        elif not left_el_is_int and not right_el_is_int:
-            list_comparison = compare(left_el, right_el)
-            if list_comparison is not None:
-                return list_comparison
+            if left_el != right_el:
+                return left_el - right_el
         else:
-            list_comparison = (
-                compare([left_el], right_el)
-                if left_el_is_int
-                else compare(left_el, [right_el])
-            )
-            if list_comparison is not None:
-                return list_comparison
-    
-    if len(left) < len(right):
-        return -1
-    elif len(left) > len(right):
-        return 1
+            if left_el_is_int:
+                left_el = [left_el]
+            if right_el_is_int:
+                right_el = [right_el]
 
-    # lists are equal
-    return 0
+            list_comparison = compare(left_el, right_el)
+            if list_comparison:
+                return list_comparison
+
+    return len(left) - len(right)
+
 
 def main():
     lines = [eval(line.strip()) for line in sys.stdin if line.strip()]
@@ -42,8 +32,11 @@ def main():
     lines.append(divider_packet2)
 
     sorted_lines = sorted(lines, key=cmp_to_key(compare))
-    sol = (sorted_lines.index(divider_packet1) + 1) * (sorted_lines.index(divider_packet2) + 1)
+    sol = (sorted_lines.index(divider_packet1) + 1) * (
+        sorted_lines.index(divider_packet2) + 1
+    )
     print(sol)
+
 
 if __name__ == "__main__":
     main()
