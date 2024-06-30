2eca0a53863cd8541cb42b690a861fcaecdefc0c
FabijanC <fabijan.corak@gmail.com>
2024-06-30 15:35:43 +0200

    Add 2017/mkday.sh

---
+++ year2017/mkday.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ $# -ne 1 ]; then
+    echo "Error: $0 <DAY_NUMBER>" 1>&2
+    exit 1
+fi
+
+day="$1"
+directory="day$day"
+
+if [ -d "$directory" ]; then
+    echo "Error: $directory" already exists
+    exit 2
+fi
+
+mkdir "$directory"
+
+for i in {1,2}; do
+    filename="$directory/part$i.awk"
+    echo "#!/usr/bin/awk -f" >"$filename"
+    chmod +x "$filename"
+done
+touch "$directory/sample.txt" "$directory/input.txt"
